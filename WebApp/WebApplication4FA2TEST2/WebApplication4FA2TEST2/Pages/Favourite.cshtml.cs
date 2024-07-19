96da660e781ca1f2e6a52b110d254c83a18d0df2
AWBoo <140343993+AWBoo@users.noreply.github.com>
2024-07-19 13:28:35 +0200

    Initial Commit

---
+++ WebApp/WebApplication4FA2TEST2/WebApplication4FA2TEST2/Pages/Favourite.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApplication4FA2TEST2.Pages
+{
+    public class FavouriteModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ WebApp/WebApplication4FA2TEST2/WebApplication4FA2TEST2/Pages/LatestNews.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApplication4FA2TEST2.Pages
+{
+    public class LatestNewsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
